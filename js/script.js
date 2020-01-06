a8b8bf95cec1287eb83c9f6461386f7991d5ad98
mathunes <matheusantunes720@gmail.com>
2020-01-06 20:44:04 -0300

    Adicao da funcao para adicionar os valores no array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 
+const op = Array(" ");
+
+const addOp = (btn) => {
+    if ("0123456789".indexOf(btn) != -1) {
+        
+        if ("+-*/".indexOf(op[op.length-1]) == -1) {
+            op[op.length-1]+=btn;
+            
+        }
+    }
+};
+
 document.querySelector("#calculator").addEventListener('click', (e) => {
     let btn = e.target.id;
 
     if (btn != "calculator" && btn != "") {
         btn = btn.substring(4);
-        console.log(btn);
+        addOp(btn);
     }
-})
+});
