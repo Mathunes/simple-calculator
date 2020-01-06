22bae39e0ff11b7bd4b81a2ad4ceb8d78334d057
mathunes <matheusantunes720@gmail.com>
2020-01-06 20:46:45 -0300

    Adicao da funcao que exibe os valores digitados no display

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 
 const op = Array(" ");
 
+const showDisplay = (btn) => {
+    document.querySelector("#calculator #display p").innerText += btn;
+};
+
 const addOp = (btn) => {
     if ("0123456789".indexOf(btn) != -1) {
         
         if ("+-*/".indexOf(op[op.length-1]) == -1) {
+
             op[op.length-1]+=btn;
-            
+            showDisplay(btn);
+
         }
-    }
 };
 
 document.querySelector("#calculator").addEventListener('click', (e) => {
