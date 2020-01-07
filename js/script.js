c9f921ffbf16ce96d715e64159cded59b1a0a1dc
mathunes <matheusantunes720@gmail.com>
2020-01-06 21:04:40 -0300

    - Adicao do funcionamento do botao '.' - Adicao do funcionamento do botao 'ce'

--- js/script.js
+++ js/script.js
@@ -14,6 +14,26 @@ const addOp = (btn) => {
             showDisplay(btn);
 
         }
+
+    }  else if (btn == "point") {
+        
+        if ("+-*/".indexOf(op[op.length-1]) == -1) {
+
+            if (op[op.length-1].indexOf('.') == -1) {
+                op[op.length-1]+='.';
+                showDisplay('.');
+            }
+
+        }
+
+    } else if (btn == "ce") {
+
+        document.querySelector("#calculator #display p").innerText = "";
+        op[op.length-1] = " ";
+        
+    } else if (btn == "ac") {
+
+    }
 };
 
 document.querySelector("#calculator").addEventListener('click', (e) => {
